2a1a36fad9de3812cbeb769af1302412cc3b4671
ON22JB <jacqbohn@aol.com>
2024-03-26 11:50:25 +0100

    Aufteilung der pin.vue in Pin Design und Standort-Entfernungs-Logik

--- src/components/pin.vue
+++ src/components/pin.vue
@@ -7,6 +7,8 @@
 </template>
 
 <script>
+import { calculateDistance } from './utils.vue';
+
 export default {
   data() {
     return {
@@ -46,37 +48,18 @@ export default {
     },
     checkProximity() {
       if (this.userLocation) {
-        const distance = this.calculateDistance(
+        const distance = calculateDistance(
           this.userLocation.latitude,
           this.userLocation.longitude,
           this.pinLocation.latitude,
           this.pinLocation.longitude
         );
-        if (distance <= 20) {
-          this.isClose = true;
-        } else {
-          this.isClose = false;
-        }
+        this.isClose = distance <= 20;
       }
     },
     onClick() {
       this.isChecked = !this.isChecked;
       this.isClose = false;
-    },
-    calculateDistance(lat1, lon1, lat2, lon2) {
-      const R = 6371; // Radius of the earth in km
-      const dLat = this.deg2rad(lat2 - lat1);
-      const dLon = this.deg2rad(lon2 - lon1);
-      const a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
-        Math.sin(dLon / 2) * Math.sin(dLon / 2);
-      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-      const d = R * c; // Distance in km
-     return d * 1000; // Convert to meters
-    },
-    deg2rad(deg) {
-     return deg * (Math.PI / 180);
     }
   }
 };
