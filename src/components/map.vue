1c6e410c7dd7486261f803abf52ce5700722dedc
ON22JB <jacqbohn@aol.com>
2024-03-28 09:18:05 +0100

    Karte Hinzugefügt

--- src/components/map.vue
+++ src/components/map.vue
@@ -1,18 +1,35 @@
-<script setup>
-var map = L.map('map').setView({lon: 0, lat: 0}, 2);
-</script>
-
 <template>
-    <div id="map"></div>
+  <div id="leaflet-map"></div>
 </template>
 
+<script>
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+export default {
+  mounted() {
+    // Initialize the map
+    this.map = L.map('leaflet-map').setView([49.35330824531996, 9.149673396841493], 16);
+
+    // Add the tile layer (OpenStreetMap)
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(this.map);
+  }
+};
+</script>
+
 <style scoped>
-#map {
+#leaflet-map {
+  min-height: 100%;
+  min-width: 100%;
+  display: flex;
+  margin:0;
   position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  z-index: -1;
+  top:0;
+  left:0;
+  right:0;
+  bottom:0;
+  z-index:0;
 }
 </style>
